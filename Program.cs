c8ccb2113c2b1a9aac3df79f54fd0f3daf96c6a7
honglin <honglin.yang@mims.com>
2019-05-04 17:09:50 +0800

    Create a new console application

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OutOfTheMaze
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
